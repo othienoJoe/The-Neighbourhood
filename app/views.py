8485fc630b6ed7603d489f167dbe09dc91218247
othienoJoe <bryo094@gmail.com>
2021-11-01 11:27:46 +0300

    installed pillow and updated the views file

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.exceptions import ObjectDoesNotExist
-from app.models import Profile
+from django.contrib.auth.decorators import login_required
+from PIL import Image
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,28 @@ def index(request):
 	except ObjectDoesNotExist:
 		return redirect('create-profile')
 	return render(request,'index.html')
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+	current_user=request.user
+	profile =Profile.objects.get(username=current_user)
+	return render(request,'profile/user_profile.html',{"profile":profile})
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+	user = User.objects.get(username=username)
+	profile =Profile.objects.get(username=user)
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+	current_user=request.user
+	if request.method=="POST":
+		form =ProfileForm(request.POST,request.FILES)
+		if form.is_valid():
+			profile = form.save(commit = False)
+			profile.username = current_user
+			profile.save()
+		return HttpResponseRedirect('/')
+	else:
+		form = ProfileForm()
+		return render(request,'profile/profile_form.html',{"form":form})
